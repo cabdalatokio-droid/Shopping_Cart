cd71cba6396096ec3741c6b70454bfa059a77348
abdalla <cabdalatokio@gmail.com>
2026-04-25 20:56:36 +0300

    im building

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header"
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Cart from "./pages/cart";
 
 const App=()=>{
   return(
-    <Header/>
+    <div>
+       <Header/>
+       <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/cart" element={<Cart/>}/>
+    
+       </Routes>
+    </div>
   )
 }
 
